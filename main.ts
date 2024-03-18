1432371a4e3c9d2cb1be7c45c97ac3ebf8bf15cf
Harshvadan7 <122847160+Harshvadan7@users.noreply.github.com>
2024-03-17 20:29:16 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+/**
+ * As you can see, the showArrow function works based on the value provided to it. 
+ * 
+ * 1 -> arrow points North
+ * 
+ * 2 -> arrow points East
+ * 
+ * 3 -> arrow points South
+ * 
+ * 4 -> arrow points West
+ * 
+ * Hence, you just need to modify the value of the 'direction' variable. Use the 'set direction to - ' block inside an if-else block to change its value based on the button pressed.
+ */
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+function showArrow (num: number) {
+    if (num == 1) {
+        basic.showArrow(ArrowNames.North)
+    } else if (num == 2) {
+        basic.showArrow(ArrowNames.East)
+    } else if (num == 3) {
+        basic.showArrow(ArrowNames.South)
+    } else if (num == 4) {
+        basic.showArrow(ArrowNames.West)
+    }
+}
+let direction = 1
+basic.forever(function () {
+    showArrow(direction)
+})
